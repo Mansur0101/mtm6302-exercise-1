d1306709103e342da70c805711ec42ffa4a317a6
Mansur <omar0133@algonquinlive.com>
2020-10-11 18:50:02 -0400

    Completes assignment

---
+++ script.js
@@ -0,0 +1,165 @@
+let main = document.querySelector('.main');
+let h2 = main.querySelector('h2')
+h2.textContent = 'By Abilmansur Omar';
+
+
+let p = main.querySelector('p');
+p.innerHTML = '&#129409';
+
+let p2 = main.querySelector('p:nth-child(2)');
+p2.innerHTML = '&#8987;';
+
+let p3 = main.querySelector('p:nth-child(3)');
+p3.innerHTML = '&#9193;';
+
+let p4 = main.querySelector('p:nth-child(4)');
+p4.innerHTML = '&#9194;';
+
+let p5 = main.querySelector('p:nth-child(5)');
+p5.innerHTML = '&#9195;';
+
+let p6 = main.querySelector('p:nth-child(6)');
+p6.innerHTML = '&#9193;';
+
+let p7 = main.querySelector('p:nth-child(7)');
+p7.innerHTML = '&#9200;';
+
+let p8 = main.querySelector('p:nth-child(8)');
+p8.innerHTML = '&#9201;';
+
+let p9 = main.querySelector('p:nth-child(9)');
+p9.innerHTML = '&#9202;';
+
+let p10 = main.querySelector('p:nth-child(10)');
+p10.innerHTML = '&#9203;';
+
+let p11 = main.querySelector('p:nth-child(11)');
+p11.innerHTML = '&#9208;';
+
+let p12 = main.querySelector('p:nth-child(12)');
+p12.innerHTML = '&#9209;';
+
+let p13 = main.querySelector('p:nth-child(13)');
+p13.innerHTML = '&#9210;';
+
+let p14 = main.querySelector('p:nth-child(14)');
+p14.innerHTML = '&#9410;';
+
+let p15 = main.querySelector('p:nth-child(15)');
+p15.innerHTML = '&#9748;';
+
+let p16 = main.querySelector('p:nth-child(16)');
+p16.innerHTML = '&#9749;';
+
+let p17 = main.querySelector('p:nth-child(17)');
+p17.innerHTML = '&#9757;';
+
+let p18 = main.querySelector('p:nth-child(18)');
+p18.innerHTML = '&#9800;';
+
+let p19 = main.querySelector('p:nth-child(19)');
+p19.innerHTML = '&#9801;';
+
+let p20 = main.querySelector('p:nth-child(20)');
+p20.innerHTML = '&#9802;';
+
+let p21 = main.querySelector('p:nth-child(21)');
+p21.innerHTML = '&#9803;';
+
+let p22 = main.querySelector('p:nth-child(22)');
+p22.innerHTML = '&#9804;';
+
+let p23 = main.querySelector('p:nth-child(23)');
+p23.innerHTML = '&#9805;';
+
+let p24 = main.querySelector('p:nth-child(24)');
+p24.innerHTML = '&#9806;';
+
+let p25 = main.querySelector('p:nth-child(25)');
+p25.innerHTML = '&#9807;';
+
+let p26 = main.querySelector('p:nth-child(26)');
+p26.innerHTML = '&#9808;';
+
+let p27 = main.querySelector('p:nth-child(27)');
+p27.innerHTML = '&#9809;';
+
+let p28 = main.querySelector('p:nth-child(28)');
+p28.innerHTML = '&#9810;';
+
+let p29 = main.querySelector('p:nth-child(29)');
+p29.innerHTML = '&#9811;';
+
+let p30 = main.querySelector('p:nth-child(30)');
+p30.innerHTML = '&#9823;';
+
+let p31 = main.querySelector('p:nth-child(31)');
+p31.innerHTML = '&#9855;';
+
+let p32 = main.querySelector('p:nth-child(32)');
+p32.innerHTML = '&#9875;';
+
+let p33 = main.querySelector('p:nth-child(33)');
+p33.innerHTML = '&#9889;';
+
+let p34 = main.querySelector('p:nth-child(34)');
+p34.innerHTML = '&#9898;';
+
+let p35 = main.querySelector('p:nth-child(35)');
+p35.innerHTML = '&#9899;';
+
+let p36 = main.querySelector('p:nth-child(36)');
+p36.innerHTML = '&#9917;';
+
+let p37 = main.querySelector('p:nth-child(37)');
+p37.innerHTML = '&#9918;';
+
+let p38 = main.querySelector('p:nth-child(38)');
+p38.innerHTML = '&#9924;';
+
+let p39 = main.querySelector('p:nth-child(39)');
+p39.innerHTML = '&#9925;';
+
+let p40 = main.querySelector('p:nth-child(40)');
+p40.innerHTML = '&#9934;';
+
+let p41 = main.querySelector('p:nth-child(41)');
+p41.innerHTML = '&#9935;';
+
+let p42 = main.querySelector('p:nth-child(42)');
+p42.innerHTML = '&#9937;';
+
+let p43 = main.querySelector('p:nth-child(43)');
+p43.innerHTML = '&#9939;';
+
+let p44 = main.querySelector('p:nth-child(44)');
+p44.innerHTML = '&#9940;';
+
+let p45 = main.querySelector('p:nth-child(45)');
+p45.innerHTML = '&#9961;';
+
+let p46 = main.querySelector('p:nth-child(46)');
+p46.innerHTML = '&#9962;';
+
+let p47 = main.querySelector('p:nth-child(47)');
+p47.innerHTML = '&#9968;';
+
+let p48 = main.querySelector('p:nth-child(48)');
+p48.innerHTML = '&#9969;';
+
+let p49 = main.querySelector('p:nth-child(49)');
+p49.innerHTML = '&#9970;';
+
+let p50 = main.querySelector('p:nth-child(50)');
+p50.innerHTML = '&#9971;';
+
+
+
+
+
+
+
+
+
+
+
